557e92af02094e1960105253560c95706580c9fc
Preston Carlton <preston.carlton@tiag.net>
2021-01-05 12:26:24 -0500

    init commit

---
+++ src/components/CounterButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button @click="add" class="counter-button">I've been clicked {{ count }} times.</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    add() {
+      this.count += 1;
+    },
+  },
+};
+</script>
+
+<style>
+.counter-button {
+    border-radius: 5px;
+    outline: none;
+    background-color: none;
+    border: 2px solid #F4D35E;
+}
+</style>
